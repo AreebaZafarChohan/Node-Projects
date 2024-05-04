f24027de1117f52b46a89133f3d5f1a732ba5da3
AreebaZafarChohan <areebazafar715@gmail.com>
2024-05-04 17:26:35 +0500

    Changes in file

--- project05_word_counter/word_counter.js
+++ project05_word_counter/word_counter.js
@@ -9,6 +9,13 @@ function countWords(inputText) {
     const words = inputText.split(/\s+/).filter(word => word.length > 0);
     return words.length;
 }
+/* Function to count characters in the input text exluding spaces.
+This function removes spaces from the input text and returns the length of
+the modified strings */
+function countCharactersWithoutSpaces(inputText) {
+    const textWithoutSpaces = inputText.replace(/\s+/g, '');
+    return textWithoutSpaces.length;
+}
 // This function takes user input through inquirer and this is a main function to run the application.
 async function mainFunc() {
     const questions = [
@@ -22,9 +29,13 @@ async function mainFunc() {
     const answers = await inquirer.prompt(questions);
     // Extacting the input text from the user's response 
     const text = answers["text"];
-    // Counting the numbers of words in the input text through countWords function 
+    // Counting the numbers of words and characters in the input text through respective functions 
     const countedWords = countWords(text);
+    const countedCharacters = text.length;
+    const countCharacterWithoutSpaces = countCharactersWithoutSpaces(text);
     console.log(chalk.cyan(`Counted Words are: ${countedWords}`));
+    console.log(chalk.yellow(`Counted Characters are: ${countedCharacters}`));
+    console.log(chalk.magenta(`Counted Characters without Spaces are: ${countCharacterWithoutSpaces}`));
 }
 // Call mainFunc to start the application 
 mainFunc();
--- project05_word_counter/word_counter.ts
+++ project05_word_counter/word_counter.ts
@@ -1,38 +1,50 @@
-import inquirer,{QuestionCollection,Answers} from "inquirer";
+import inquirer, { QuestionCollection, Answers } from "inquirer";
 import chalk from "chalk";
 
 /* Function to count words in the input text.
 This function takes input and slplit it into words based on whitespaces 
 then filtering the empty words and returning non empty words. */
 
-function countWords(inputText : string): number {
-    const words : string[] = inputText.split(/\s+/).filter(word => word.length > 0);
+function countWords(inputText: string): number {
+    const words: string[] = inputText.split(/\s+/).filter(word => word.length > 0);
     return words.length;
 }
 
+/* Function to count characters in the input text exluding spaces.
+This function removes spaces from the input text and returns the length of 
+the modified strings */
+function countCharactersWithoutSpaces(inputText: string): number {
+    const textWithoutSpaces: string = inputText.replace(/\s+/g, '');
+    return textWithoutSpaces.length;
+}
 // This function takes user input through inquirer and this is a main function to run the application.
 
 async function mainFunc(): Promise<void> {
-    const questions : QuestionCollection = [
-        {       
-            type : "input",
-            name : "text",
-            message : "Enter the text to count words:", 
+    const questions: QuestionCollection = [
+        {
+            type: "input",
+            name: "text",
+            message: "Enter the text to count words:",
         },
     ];
-    
+
     // Prompting the user for input and waiting for their response. 
 
-    const answers : Answers = await inquirer.prompt(questions);
+    const answers: Answers = await inquirer.prompt(questions);
 
     // Extacting the input text from the user's response 
 
-    const text : string = answers["text"];
+    const text: string = answers["text"];
 
-    // Counting the numbers of words in the input text through countWords function 
-    const countedWords : number = countWords(text);
+    // Counting the numbers of words and characters in the input text through respective functions 
+    const countedWords: number = countWords(text);
+    const countedCharacters: number = text.length;
+    const countCharacterWithoutSpaces: number = countCharactersWithoutSpaces(text);
 
     console.log(chalk.cyan(`Counted Words are: ${countedWords}`));
+    console.log(chalk.yellow(`Counted Characters are: ${countedCharacters}`));
+    console.log(chalk.magenta(`Counted Characters without Spaces are: ${countCharacterWithoutSpaces}`));
+
 }
 
 // Call mainFunc to start the application 
